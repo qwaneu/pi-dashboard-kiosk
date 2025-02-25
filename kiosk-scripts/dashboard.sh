cd9d50aa1505fe624a4aa2e312248b14faba10a7
rob <rob@qwan.eu>
2025-02-25 16:58:30 +0100

    monitoring dashboard

--- kiosk-scripts/dashboard.sh
+++ kiosk-scripts/dashboard.sh
@@ -1,11 +1,16 @@
 config_dir=~/.config/pipeline-kiosk
 tabs_file=${config_dir}/tabs.txt
 
-chromium-browser $(cat ${tabs_file}) \
-       --kiosk \
-       --noerrdialogs \
-       --disable-infobars \
-       --no-first-run \
-       --ozone-platform=wayland \
-       --enable-features=OverlayScrollbar \
-       --start-maximized
+while true
+do 
+        chromium-browser $(cat ${tabs_file}) \
+               --kiosk \
+               --noerrdialogs \
+               --disable-infobars \
+               --no-first-run \
+               --ozone-platform=wayland \
+               --enable-features=OverlayScrollbar \
+               --start-maximized &
+        pid=$(pgrep chromium | head -1)
+        wait $pid
+done
